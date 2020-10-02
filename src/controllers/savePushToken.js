8976067232c5737502f04b374dd5394d87604f41
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-02 22:30:51 +0100

    .

--- src/controllers/savePushToken.js
+++ src/controllers/savePushToken.js
@@ -9,6 +9,6 @@ module.exports = async (req, res) => {
 		return res.status(409).json({status: 'error', message: 'User not found', data: ''});
 	} else {
 		const data = await updateUser({_id, token});
-		return res.status(200).json({status: 'ok', message: 'Token Saved', data});
+		return res.status(200).json({status: 'ok', message: 'Token Saved', data: data.user});
 	}
 };
